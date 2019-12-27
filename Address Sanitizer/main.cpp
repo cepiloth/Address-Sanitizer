3ec4240fa629c4614b45284ad401088fa44b1fca
jj <lim10260@nate.com>
2019-12-27 12:14:18 +0900

    Update ASAN

--- Address Sanitizer/main.cpp
+++ Address Sanitizer/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+void buffer_overflow() {
+	int* ch = new int[100];
+
+	ch[100] = 1;
+}
+
+void use_after_free() {
+	int* ch = new int[100];
+	delete[] ch;
+
+	delete[] ch;
+}
+
+void uninitialize() {
+	int* ch = nullptr;
+
+	*ch = 1;
+}
+
 int main() {
+
+	uninitialize();
 
 	return 0;
 }
